0746850de7f4da81effbd24b3e01a44d00665788
wonik <twer4774@gmail.com>
2021-10-25 13:42:24 +0900

    simple_gitaction Init

---
+++ simple_gitaction/src/main/java/walter/unit/gitaction_example/GitactionExampleApplication.java
@@ -0,0 +1,13 @@
+package walter.unit.gitaction_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionExampleApplication.class, args);
+	}
+
+}
